03d474da3e7dafa20d52007159d3c86fda4b0f1c
Amanda Ekaratih Siswojo <aes793@nyu.edu>
2018-05-03 23:35:06 -0400

    Added speech bubble, changed layout, added pause

---
+++ Assets/Scripts/PauseButton.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseButton : MonoBehaviour {
+
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    void Update()
+    {
+     
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+        
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f; //sets time back to normal
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("loading menu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("quitting");
+    }
+
+
+}
---
+++ Assets/Scripts/PeerBehavior.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeerBehavior : MonoBehaviour {
+
+    private bool disabled;
+    public float walkSpeed;
+    public bool verticalWalk;
+    public float speechTime;
+
+    public GameObject speechBub;
+    
+
+    // Use this for initialization
+    public void Start () {
+        disabled = false;
+     
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+        //making the enemy move left to right or up and down
+
+        if (disabled == false)
+        {
+            if (verticalWalk == true) //if walking vertical is true do this 
+            {
+                transform.Translate(new Vector3(0, walkSpeed, 0) * Time.deltaTime);
+            }
+            else //if not then walk horizontally
+            {
+                transform.Translate(new Vector3(walkSpeed, 0, 0) * Time.deltaTime);
+            }
+        }
+    }
+
+    public void DisablePeers()
+    { //this gets called by the player
+        disabled = true;
+        Invoke("ResetDisabled", 5.0f); //after 5 seconds call ResetDisabled()
+    }
+
+    void ResetDisabled()
+    {
+        disabled = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //if the enemy collide with game objects taged Immovables then mirror movement
+        if (collision.gameObject.tag == "Immovables")
+        {
+            walkSpeed *= -1;
+        }
+        
+        switch (collision.gameObject.tag)
+        {
+            case "Player":
+                GameObject newSpeech = Instantiate(speechBub);
+                newSpeech.transform.position = new Vector2(transform.position.x + 0.5f, transform.position.y + 1.5f);
+
+                Destroy(newSpeech, speechTime);
+                
+                Debug.Log ("TOUCHING");
+                break;
+        }
+    }
+}
